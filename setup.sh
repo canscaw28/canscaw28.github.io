cebbaa9546ae557e504ed932d8f9c971f4a35d5c
Craig Weiss <craigweiss28@gmail.com>
2020-12-06 01:07:06 -0800

    brew check

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-# Install Homebrew
-echo Installing Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install/Update Homebrew
+echo Checking if Homebrew is installed
+if [[ $(command -v brew) == "" ]]; then
+  echo "Homebrew not found; Installing Homebrew."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "Homebrew already installed; Updating Homebrew."
+  brew update
+fi
