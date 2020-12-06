61f8dd5e448ec014f05d80753314d323e7e70630
Craig Weiss <craigweiss28@gmail.com>
2020-12-06 01:20:16 -0800

    formatting

--- setup.sh
+++ setup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# setup color formatting
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+WHITE='\033[1;37m'
+NC='\033[0m'
+
 # Install/Update Homebrew
-echo Checking if Homebrew is installed
+printf "${BLUE}==>${WHITE} Checking if Homebrew is installed\n${NC}"
 if [[ $(command -v brew) == "" ]]; then
-  echo "Homebrew not found; Installing Homebrew."
+  printf "  Homebrew not found.\n  Installing Homebrew.\n"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
-  echo "Homebrew already installed; Updating Homebrew."
+  printf "  Homebrew already installed.\n  Updating Homebrew.\n"
   brew update
 fi
